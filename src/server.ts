3d047b0cf948b62aadb903956cbc6befcb225b76
pedro-monte <pedroivomonte@gmail.com>
2025-09-03 09:02:27 -0300

    UPDATE: added some transactions

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,24 @@ const app = fastify();
 // GET, POST, PUT, DELETE, PATCH
 
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*');
+  /*const test = await knex('sqlite_schema').select('*');
   console.log(test);
-  return { message: 'Hello World' };
+  return { message: 'Hello World' };*/
+
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'transação de teste',
+      amount: 1000,
+    })
+    .returning('*');
+
+  const transaction2 = await knex('transactions')
+    .where('amount', 1000)
+    .select('*');
+
+  console.log(transaction2);
+  return transaction;
 });
 
 app.listen({ port: 3333 }).then(() => {
