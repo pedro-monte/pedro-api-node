96df5480b1513950b93336bc4434eec7ad898615
pedro-monte <pedroivomonte@gmail.com>
2025-09-04 21:52:03 -0300

    UPDATE: added .dot.env

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { Knex, knex as setupKnex } from 'knex';
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './database/app.db',
+    filename: process.env.DATABASE_URL || './database/app.db',
   },
   useNullAsDefault: true,
   migrations: {
